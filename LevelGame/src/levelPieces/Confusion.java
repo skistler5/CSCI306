a0fb7d4c6a64c624fe8932d541120dbb4a8a9a2d
Ellie McCabe <ellie@Ellies-MacBook-Air.local>
2018-02-06 17:13:26 -0700

    add finished confusion

---
+++ LevelGame/src/levelPieces/Confusion.java
@@ -0,0 +1,23 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+
+public class Confusion implements Drawable{
+	static char symbol = '?';
+	private int location;
+
+	public Confusion (int location) {
+		super();
+		this.location = location;
+	}
+	
+	@Override
+	public void draw() {
+		System.out.println(symbol);
+	}
+
+	public int getLocation() {
+		return location;
+	}
+
+}
