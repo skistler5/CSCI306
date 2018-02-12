5fc6c33c9f3a46ec0a7e298a9b4823b171164b1a
Stephen Kistler <skistler@mymail.mines.edu>
2018-02-12 15:34:22 -0700

    created testmovingpieces method for the random movement

--- LevelGame/src/jUnit/TestMovingPieces.java
+++ LevelGame/src/jUnit/TestMovingPieces.java
@@ -1,5 +1,40 @@
 package jUnit;
 
-public class TestMovingPieces {
+import static org.junit.Assert.*;
+import org.junit.Test;
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import levelPieces.Rando;
+import levelPieces.LifeBox;
+import gameEngine.Player;
+import levelPieces.Obstacle;
+import levelPieces.LevelEngine;
 
+public class TestMovingPieces {
+	@Test
+	public void testRandomMovement(){
+		int count = 0;
+		Drawable[] pieces = new Drawable[GameEngine.BOARD_SIZE];
+		LevelEngine levels = new LevelEngine();
+		
+		//set some other pieces onto the board
+		for(int i = 8; i < 14; i++){
+			pieces[i] = new Obstacle(i);
+		}
+		
+		//lifebox obj set to place 17
+		LifeBox lb = new LifeBox(17);
+		
+		Drawable [] returnB = new Drawable[GameEngine.BOARD_SIZE];
+		returnB = levels.getBoard();
+		for(int i = 0; i < 21; i++){
+			lb.move(pieces, i);
+			if(pieces[i] != returnB[i]) count++;
+			System.out.println(pieces[i] + "= " + returnB[i]);
+			
+		}
+		
+		assert(count > 1);
+	}
 }
